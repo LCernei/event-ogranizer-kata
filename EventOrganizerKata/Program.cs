6691b0c1bef863a46ca20d65b1ace9efa1b695c5
Liviu Cernei <cernei.liviu@gmail.com>
2019-10-02 12:10:40 +0300

    new project initial files

---
+++ EventOrganizerKata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventOrganizerKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
